a1feb65ececebe834d0f165dabca4bc9a5f643c1
hsjiang <b98150@gmail.com>
2017-07-22 23:35:29 +0800

    Add raw data

--- parser.js
+++ parser.js
@@ -25,24 +25,50 @@ function parse_csv(file,name,output,cb){
       cb(output)
   })
 }
+function parse_fac(file,name,output,cb){
+  csv()
+  .fromFile(file)
+  .on('json',(jsonObj)=>{
+     if(!jsonObj["102"])
+       return
+     if(!output[jsonObj["CODE3"]])
+       return
+     output[jsonObj["CODE3"]][name+"102"]=jsonObj["102"]
+     output[jsonObj["CODE3"]][name+"103"]=jsonObj["103"]
+     output[jsonObj["CODE3"]][name+"104"]=jsonObj["104"]
+  })
+  .on('done',(error)=>{
+      cb(output)
+  })
+}
 parse_csv('./csv/power/102_PowerSum.csv',"102_",[],(output)=>{
-  parse_csv('./csv/power/103_PowerSum.csv',"103_",output,(out)=>{
-    parse_csv('./csv/power/104_PowerSum.csv',"104_",out,(ou)=>{
-      let title=""
-      let flag =true
-      for(var a in ou){
-        let ln = ""
-        if (Object.keys(ou[a]).length!=39)
-          continue
-        for(var s in ou[a]){
-          if(flag)
-            title+=s+','
-          ln += ou[a][s] + ','
-        }
-        console.log(ln)
-        flag=false
-      }
-      console.log(title)
+  parse_csv('./csv/power/103_PowerSum.csv',"103_",output,(output)=>{
+    parse_csv('./csv/power/104_PowerSum.csv',"104_",output,(output)=>{
+      parse_fac('./csv/factory/101_104_FactoryCount.csv',"Factory_cnt_",output,(output)=>{
+        parse_fac('./csv/factory/101_104_FactoryEmployee.csv',"Factory_employee_",output,(output)=>{
+          parse_fac('./csv/factory/101_104_FactoryTurnover.csv',"Factory_to_",output,(output)=>{
+            parse_fac('./csv/factory/101_104_Turnover_Employee.csv',"To_employee_",output,(output)=>{
+              parse_fac('./csv/factory/101_104_Turnover_Factory.csv',"To_factory_",output,(ou)=>{
+                let title=""
+                let flag =true
+                for(var a in ou){
+                  let ln = ""
+                  if (Object.keys(ou[a]).length!=54)
+                    continue
+                  for(var s in ou[a]){
+                    if(flag)
+                      title+=s+','
+                    ln += ou[a][s] + ','
+                  }
+                  console.log(ln)
+                  flag=false
+                }
+                console.log(title)
+              })
+            })
+          })
+        })
+      })
     })
   })
 })
