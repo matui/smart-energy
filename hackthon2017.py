3412fd9a41a6d73ec83b82a5c47f3efb11af588d
henryNTUEE <b02901051@ntu.edu.tw>
2017-07-22 13:40:09 +0800

    new

--- hackthon2017.py
+++ hackthon2017.py
@@ -17,19 +17,17 @@ with open(sys.argv[1], 'r') as csvfile:
          my_rows.append(row)
     for idx,row in enumerate(my_rows):
         if idx!=0:
-            for slot in range(0,8):
-                features.append((row[5+slot:8+slot]))
-                answers.append(float(row[8+slot]))
+            for slot in range(0,9):
+                if float(row[5+slot])==0.0 and float(row[6+slot])==0.0 and float(row[7+slot])==0.0:
+                    continue
+                features.append([float(x)/47840395.93 for x in row[5+slot:8+slot]])
+                answers.append(float(row[8+slot])/47840395.93)
 
 for idx,row in enumerate(features):
     print row,answers[idx]
 
 model = Sequential()
-model.add(Dense(200, input_dim=3, activation='relu'))
-model.add(BatchNormalization())
-model.add(Dense(80, activation='relu'))
-model.add(BatchNormalization())
-model.add(Dense(40, activation='relu'))
+model.add(Dense(100, input_dim=3, activation='relu'))
 model.add(BatchNormalization())
 model.add(Dense(20, activation='relu'))
 model.add(BatchNormalization())
@@ -37,8 +35,8 @@ model.add(Dense(1, activation='sigmoid'))
 
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-model.fit(features, answers, epochs=150, batch_size=10)
+model.fit(features, answers, epochs=10, batch_size=10)
 
-predictions = model.predict(X)
-rounded = [round(x[0]) for x in predictions]
-print rounded
+predictions = model.predict(features,batch_size=10)
+for x in predictions:
+    print x[0]*47840395.93
