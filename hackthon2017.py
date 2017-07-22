3b5fd9da92e281855a0dbd183598d59b389b8547
henryNTUEE <b02901051@ntu.edu.tw>
2017-07-22 20:17:07 +0800

    final

--- hackthon2017.py
+++ hackthon2017.py
@@ -8,9 +8,13 @@ import numpy as np
 
 np.random.seed(1)
 
+#Training Data
 features = []
 answers = []
-
+county = []
+town = []
+code = []
+testers = []
 with open(sys.argv[1], 'r') as csvfile:
     all_rows = csv.reader(csvfile)
     my_rows = []
@@ -18,65 +22,51 @@ with open(sys.argv[1], 'r') as csvfile:
          my_rows.append(row)
     for idx,row in enumerate(my_rows):
         if idx!=0:
-            for slot in range(0,7):
-                if float(row[5+slot])==0.0 and float(row[6+slot])==0.0 and float(row[7+slot])==0.0:
-                    continue
-                features.append([float(x)/10000000.0 for x in row[5+slot:10+slot]])
-                answers.append(float(row[10+slot])/10000000.0)
-with open(sys.argv[2], 'r') as csvfile:
-    all_rows = csv.reader(csvfile)
-    my_rows = []
-    for row in all_rows:
-         my_rows.append(row)
-    for idx,row in enumerate(my_rows):
-        if idx!=0:
-            for slot in range(0,7):
-                if float(row[5+slot])==0.0 and float(row[6+slot])==0.0 and float(row[7+slot])==0.0:
-                    continue
-                features.append([float(x)/10000000.0 for x in row[5+slot:10+slot]])
-                answers.append(float(row[10+slot])/10000000.0)
+            county.append(row[1])
+            town.append(row[2])
+            code.append(row[0])
+            for slot in range(0,24):
+                features.append([float(x)/10000000.0 for x in row[3+slot:15+slot]])
+                answers.append(float(row[15+slot])/10000000.0)
+            testers.append([float(x)/10000000.0 for x in row[27:-1]])
 
-with open(sys.argv[3], 'r') as csvfile:
-    all_rows = csv.reader(csvfile)
-    my_rows = []
-    for row in all_rows:
-         my_rows.append(row)
-    for idx,row in enumerate(my_rows):
-        if idx!=0:
-            for slot in range(0,7):
-                if float(row[5+slot])==0.0 and float(row[6+slot])==0.0 and float(row[7+slot])==0.0:
-                    continue
-                features.append([float(x)/10000000.0 for x in row[5+slot:10+slot]])
-                answers.append(float(row[10+slot])/10000000.0)
 
-#for idx,row in enumerate(features):
-#    print row,answers[idx]
+
+features = np.asarray(features)
+answers = np.asarray(answers)
+testers = np.asarray(testers)
+print "features",np.shape(features)
+print "answers",np.shape(answers)
+print "testers",np.shape(testers)
 
 model = Sequential()
-model.add(Dense(500, input_dim=5, activation='relu'))
-model.add(BatchNormalization())
-model.add(Dropout(0.13))
-model.add(Dense(300, activation='relu'))
-model.add(BatchNormalization())
-model.add(Dropout(0.11))
-model.add(Dense(100, activation='relu'))
-model.add(BatchNormalization())
-model.add(Dense(50, activation='relu'))
-model.add(BatchNormalization())
-model.add(Dense(10, activation='relu'))
-model.add(BatchNormalization())
-model.add(Dense(1, activation='sigmoid'))
+model.add(Dense(400, input_dim=12,kernel_initializer='normal', activation='relu'))
+model.add(Dense(200, input_dim=12,kernel_initializer='normal', activation='relu'))
+model.add(Dense(100,kernel_initializer='normal', activation='relu'))
+model.add(Dense(50,kernel_initializer='normal', activation='relu'))
+model.add(Dense(25,kernel_initializer='normal', activation='relu'))
+model.add(Dense(1, kernel_initializer='normal'))
+model.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy'])
+model.fit(features, answers, epochs=10, batch_size=32)
 
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+predictions = model.predict(testers,batch_size=32)
+for i,x in enumerate(predictions):
+    print county[i],town[i],code[i],int(x[0]*10000000)
 
-model.fit(features, answers, epochs=10, batch_size=10)
+with open(sys.argv[2],'w') as w:
+    for i,x in enumerate(predictions):
+        s = county[i] + "," + town[i] + "," + str(int(x[0]*10000000))
+        w.write(s)
+        w.write("\n")
 
-predictions = model.predict(features,batch_size=32)
+'''
 i=0
 s=''
 for x in predictions:
-    if i%7==0:
+    if i%24==0:
         print s
+        print " "
         s=''
-    s+='{0:10} , '.format(x[0]*10000000.0)
+    s+='{0:10} , '.format(int(x[0]*10000000))
     i+=1
+'''
